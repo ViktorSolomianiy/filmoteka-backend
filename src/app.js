df8ec1d9b956571ccf412cfb202d6d6dbb2114de
ViktorSolomianiy <viktorsolomianiy@gmail.com>
2023-08-22 13:45:07 +0300

    create backend

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 const fastify = require('fastify')();
 const typeormConfig = require('./typeorm-config');
 
-typeormConfig
-  .initialize()
-  .then((connection) => {
+const connectToDatabase = async () => {
+  try {
+    const connection = await typeormConfig.initialize();
     console.log('Connected to the database');
-  })
-  .catch((err) => {
-    console.log('Error conncetion =', err);
-  });
+    return connection;
+  } catch (err) {
+    console.error('Error connecting to the database:', err.message);
+    setTimeout(connectToDatabase, 5000);
+  }
+};
+
+connectToDatabase();
 
 fastify.get('/', async (request, reply) => {
   return { message: 'Hello world' };
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);
--- src/typeorm-config.js
+++ src/typeorm-config.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const typeormConfig = new DataSource({
-  type: 'postgres',
+  type: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
-  logging: true,
+  logging: false,
 });
 
 module.exports = typeormConfig;
